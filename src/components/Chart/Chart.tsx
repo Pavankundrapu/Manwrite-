e5da16cd5495e69e10a9e6b090511813e3aab344
Pavan Kumar Kundrapu <iampavanhere@gmail.com>
2024-09-02 11:41:58 +0530

    first mantime commit

---
+++ src/components/Chart/Chart.tsx
@@ -0,0 +1,20 @@
+import { AreaChart } from '@mantine/charts';
+import { data } from './data';
+
+export function Chart() {
+  return (
+    <AreaChart
+      h={300}
+      data={data}
+      dataKey="date"
+      type="stacked"
+      series={[
+        { name: 'Apples', color: 'indigo.6' },
+        { name: 'Oranges', color: 'blue.6' },
+        { name: 'Tomatoes', color: 'teal.6' },
+      ]}
+    />
+  );
+}
+//i want to add width to the chart and give me code
+
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,13 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import {Chart} from '../components/Chart/Chart';
 
 export function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <Chart />
     </>
   );
 }
